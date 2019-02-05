512b3d73035ef6b7b4d7d812448ee8f219fb82cc
mark baruch <mbaruch28@gmail.com>
2019-02-05 20:12:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -3,7 +3,7 @@ def welcome
 end
 
 def deal_card
-  rand(1..11)
+  rand(1..11)  
   # code #deal_card here
 end
 
@@ -18,22 +18,35 @@ def prompt_user
 end
 
 def get_user_input
+  get.chomp.strip
   # code #get_user_input here
 end
 
 def end_game
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
   # code #end_game here
 end
 
 def initial_round
+  round_one = deal_card + deal_card
+  display_card_total(round_one)
+  return round_one
   # code #initial_round here
 end
 
 def hit?
+  prompt_user
+  user_input = get_user_input
+  if user_input = "hi"
+    card_total += deal_card
+  elsif user_input == "s"
+    card_total
   # code hit? here
 end
 
 def invalid_command
+  puts "Sorry, not a valid command"
+  get_user_input
   # code invalid_command here
 end
 
